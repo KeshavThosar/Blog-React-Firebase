a082f20613cc4c2ee9df62f3cc78eac58155c8db
KeshavThosar <keshavjaythosar@gmail.com>
2025-10-08 13:57:40 +0530

    resetted to scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import { useFirebaseAppContext } from './firebaseConfig'
-
-
-
 function App() {
-  const { db, auth } = useFirebaseAppContext();
-  console.log(db);
-  console.log(auth);
   return (
     <>
     </>
