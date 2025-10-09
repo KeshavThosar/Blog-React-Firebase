e6631a827c117a80602029ccf919627ab0b4104a
KeshavThosar <keshavjaythosar@gmail.com>
2025-10-09 10:22:06 +0530

    Removed unused code

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Suspense } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useFirebaseAppContext } from "../firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -5,14 +5,11 @@ export default function BlogCard({ blog: { doc, title, content } }) {
     <div className="max-w-md my-3 p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow">
       <h3 className="text-xl font-bold text-gray-800 mb-3">
         <Link to={`/blogs/${doc}`} >
-          {title ?? 'Understanding React Hooks'}
+          { title }
         </Link>
       </h3>
       <p className="text-gray-600 leading-relaxed">
-        { content ?? 
-        `React Hooks are functions that let you use state and other React features 
-        without writing a class. They provide a more direct API to the React concepts 
-        you already know...` }
+        { content }
       </p>
     </div>
   );
